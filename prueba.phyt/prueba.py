9b03e10e3f5a90e3ab23a250593218e92eb1f28f
El1anmartinez <elianmartinezherrera1@gmail.com>
2026-04-14 11:44:26 -0400

    primer commit

---
+++ prueba.phyt/prueba.py
@@ -0,0 +1,8 @@
+#Pedir 2 numeros al usuario y mostrar su suma y resta
+# numero1=int(input("Ingrese un numero "))
+# numero2=int(input("ingrese otro numero "))
+# resultadosuma=numero1+numero2
+# resultadoresta=numero1-numero2
+# print("El resultado de la suma es ",resultadosuma)
+# print("El resultado de la resta es ",resultadoresta)
+
