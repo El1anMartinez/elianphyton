46b73dc443b7faa83f37a01bd86585b17f68b3eb
El1anMartinez <elianmartinezherrera1@gmail.com>
2026-04-21 13:15:31 -0400

    ejercicios 1

---
+++ prueba.phyt/inicio.py
@@ -0,0 +1,61 @@
+# titulo="clima actual" #tipo 
+# diadelmes=16 #int numero entero
+# mes=4 #int numero entero
+# temp=18.6 #float numero decimal
+# llueve=False
+
+# print(f"{titulo}")
+# print(f"fecha hoy: {diadelmes} {mes}")
+# print(f"temperatura hoy {temp} grados")
+
+# if llueve :
+#     print ("saco el paragua")
+# else:      
+#     print("no saco el paraguas")
+
+
+# num=int(input("Ingrese un numero: "))
+# for i in range(num):
+#     print(f"{i+1} Hola bro")
+
+# nombre=input("Ingrese su nombre: ")
+# vocales=0
+# consonantes=0
+# for i in nombre:
+#     if i in "aeiouAEIOU":
+#         vocales=vocales+1
+#     elif i==" ":
+#         print()
+#     else:
+#         consonantes=consonantes+1
+# print(f"La cantidad de consonantes son {consonantes}")
+# print(f"La cantidad de vocales son {vocales}")
+
+#Control+F Reemplazar
+
+# ent1="giovani"
+# equipo1=6
+
+# ent2="sabrina"
+# equipo2=4
+
+# print ("El entrenador", ent1 ,"tiene ", equipo1, "Pokemons")
+# print ("El entrenador "+ ent1 + " tiene " + str (equipo1) + " Pokemons")
+# print (f"El entrenador {ent1} tiene {equipo1}  Pokemons")
+
+# name= "Eva"
+# nom=" Elian Martinez "
+# print(nom.strip())
+# print (nom.lower())
+# print(nom.upper())
+# print (nom.replace("Martinez", "Mussolini"))
+# print (nom.find("Martinez"))
+# print(nom.split())
+
+# contraseña=("Shazam".lower()) or ("Shazam".upper())
+# contra=input("ingrese la contraseña ")
+# if contraseña==contra.lower() or contra.upper() :
+#     print("Acceso concedido")
+# else:
+#     print("acceso denegado")
+
